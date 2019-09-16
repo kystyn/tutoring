d13d0047cf6056e109e76fb54a6ca4c6f6c80da2
kystyn <konstantindamaskinskiy@gmail.com>
2019-09-17 00:04:58 +0300

    ачал 6 задание

---
+++ Екатерина/task6/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+queue<int> mergeQueues( queue<int> &q1, queue<int> &q2 )
+{
+    queue<int> q;
+    int n = 0, size = q1.size() + q2.size();
+
+
+    while (n < size)
+    {
+        //if (q1.front() < q2.хоро)
+    }
+}
+
+int main()
+{
+    return 0;
+}
