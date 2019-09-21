d058a6bdcf34b8df44a220f7c5c776651ff7c06d
kystyn <konstantindamaskinskit@gmail.com>
2019-09-22 01:16:37 +0300

    done 6 task

--- Екатерина/task6/main.cpp
+++ Екатерина/task6/main.cpp
@@ -1,21 +1,56 @@
 #include <iostream>
-#include <queue>
+#include <sstream>
+#include <list>
 
 using namespace std;
 
-queue<int> mergeQueues( queue<int> &q1, queue<int> &q2 )
+using queue = list<int>;
+
+void push( queue &q, int el )
 {
-    queue<int> q;
-    int n = 0, size = q1.size() + q2.size();
+    q.push_back(el);
+}
 
+//void pop( queue &q, int &el )
+//{
+//    el = q.front();
+//    q.pop_front();
+//}
 
-    while (n < size)
-    {
-        //if (q1.front() < q2.хоро)
-    }
+void scanQueue( queue &q )
+{
+    string str;
+    getline(cin, str);
+
+    istringstream iss(str);
+    string el;
+
+    while (iss >> el)
+        push(q, stoi(el));
+
+}
+
+void printQueue( queue const &q )
+{
+    for (auto e : q)
+        cout << e << ' ';
+    cout << '\n';
 }
 
 int main()
 {
+    queue q1, q2;
+
+    scanQueue(q1);
+    scanQueue(q2);
+
+    q1.merge(q2);
+    q1.sort([]( int v1, int v2 )
+    {
+        return v1 > v2;
+    });
+
+    printQueue(q1);
+
     return 0;
 }
