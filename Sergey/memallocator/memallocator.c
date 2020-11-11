48483d1640899a7d523a1a3e59e374aa664044c4
kystyn <konstantindamaskinskiy@gmail.com>
2020-11-11 21:41:01 +0300

    Memallocator improvements: NULL checks and error fix

--- Sergey/memallocator/memallocator.c
+++ Sergey/memallocator/memallocator.c
@@ -4,103 +4,153 @@
 #include "memallocator.h"
 
 // pointer to the block list begin
-block_t *memList;
 
-int meminit(void *pMemory, int size)
-{
-    memList = pMemory;
 
-    if (size < sizeof(block_t) || pMemory == NULL)
-        return 0;
+block_t* memList;
+int memSize = 0;
+bool_t init = FALSE;
+
+int memgetminimumsize() {
+  return sizeof(block_t) + sizeof(int);
+}
+
+int memgetblocksize() {
+  return sizeof(block_t) + sizeof(int);
+}
+
+int meminit(void *pMemory, int size){
+
+    if (size < memgetminimumsize() || pMemory == NULL || init == TRUE)
+        return ERROR;
+    memList = (block_t*)pMemory;
+    memSize = size;
 
     memList->prev = NULL;
     memList->next = NULL;
-    memList->size = size - sizeof(block_t);
-    return 1;
+    memList->size = memSize - sizeof(block_t);
+    init = TRUE;
+    return OK;
 }
 
-void memdone()
-{
-    // ???
+void* memalloc(int size) {
+  if (size <= 0 || memList == NULL)
+    return NULL;
+
+  block_t* block;
+  block_t* best = NULL;
+  int bestSize = INT_MAX;
+
+  // find best block
+  for (block = memList; block != NULL; block = block->next)
+    if (block->size - size >= 0 && block->size - size < bestSize - size) {
+      best = block;
+      bestSize = best->size;
+    }
+
+  if (best == NULL)
+    return NULL;
+
+  // separate block
+  block_t* returnToUser = best + 1;
+
+  block_t* newBlock;
+  newBlock = (block_t*)((char*)(best)+sizeof(block_t) + size);
+
+  if (best == memList)
+      memList = newBlock;
+
+  newBlock->size = best->size - size - sizeof(block_t);
+  if (newBlock->size <= 0)
+  {
+      if (newBlock == memList)
+          memList = NULL;
+      best->next = NULL;
+      return returnToUser;
+  }
+
+  if (best->prev != NULL) {
+    best->prev->next = newBlock;
+  }
+  newBlock->prev = best->prev;
+
+  if (best->next != NULL)
+    best->next->prev = newBlock;
+  newBlock->next = best->next;
+
+  best->size = size;
+
+  return returnToUser;
 }
 
-void * memalloc(int size)
-{
-    if (size <= 0)
-        return NULL;
+void memfree(void* p) {
+    block_t* block;
 
-    block_t *block;
-    block_t *best = NULL;
-    int bestSize = INT_MAX;
+    block_t* newBlock = (block_t*)p - 1;
 
-    // find best block
-    for (block = memList; block != NULL; block = block->next)
-        if (block->size - size >= 0 && block->size - size < bestSize - size)
-        {
-            best = block;
-            bestSize = best->size;
+    if (memList == NULL)
+    {
+        memList = (block_t *)p - 1;
+        memList->next = NULL;
+        memList->prev = NULL;
+        return;
+    }
+
+    // suppose that memList is the leftist free block
+    if ((block_t*)p < memList)
+    {
+        block = memList;
+        // reassign memList
+        memList = (block_t *)p - 1;
+        memList->next = block;
+        assert(block->prev == NULL);
+        memList->prev = NULL; // == block->prev
+    }
+    else
+    {
+        //search for previous block
+        for (block = memList;
+             block != NULL &&
+             block->next != NULL &&
+             (void*)block->next < p;
+             block = block->next);
+
+        // error
+        if (block == NULL)
+            return;
+
+        block_t* oldNext = block->next;
+
+        block->next = newBlock;
+        newBlock->prev = block;
+
+        newBlock->next = oldNext;
+        if (oldNext != NULL)
+            oldNext->prev = newBlock;
+
+        // cannot be executed from the first `if` branch
+        if ((char*)newBlock->prev + sizeof(block_t) + newBlock->prev->size == newBlock) {
+
+          newBlock->prev->next = newBlock->next; // ok
+          if (newBlock->next != NULL)
+            newBlock->next->prev = newBlock->prev;
+          newBlock->prev->size += sizeof(block_t) + newBlock->size; // ok
+          newBlock = newBlock->prev;
         }
+    }
 
-    if (best == NULL)
-        return NULL;
-
-    // separate block
-    block_t *returnToUser = best + 1;
-
-    block_t *newBlock;
-    newBlock = (char*)(best) + sizeof (block_t) + size;
-
-    if (best->prev != NULL)
-        best->prev->next = newBlock;
-    newBlock->prev = best->prev;
-
-    if (best->next != NULL)
-        best->next->prev = newBlock;
-    newBlock->next = best->next;
-
-    newBlock->size = best->size - size - sizeof (block_t);
-
-    return returnToUser;
-}
-
-void memfree(void *p)
-{
-    block_t *block;
-
-    for (block = memList; (void*)block->next < p; block = block->next)
-        ;
-    block_t *oldNext = block->next;
-    block_t *newBlock = (char *)p - sizeof (block_t);
-
-    block->next = newBlock;
-    newBlock->prev = block;
-
-    newBlock->next = oldNext;
-    oldNext->prev = newBlock;
-
+    // free blocks merge
     // NULL checks
-    if (newBlock->prev + sizeof (block_t) + newBlock->prev->size == newBlock)
-    {
-        newBlock->prev->next = newBlock->next;
-        newBlock->next->prev = newBlock->prev;
-        newBlock->prev->size += sizeof (block_t) + newBlock->size;
-        newBlock = newBlock->prev;
-    }
 
-    if (newBlock + sizeof(block_t) + newBlock->size == newBlock->next)
-    {
-        newBlock->next->next->prev = newBlock;
-        newBlock->next = newBlock->next->next;
-        newBlock->size += sizeof (block_t) + newBlock->next->size;
+    if ((char*)newBlock + sizeof(block_t) + newBlock->size == newBlock->next) {
+        if (newBlock->next->next != NULL)
+            newBlock->next->next->prev = newBlock;
+      newBlock->next = newBlock->next->next;
+      newBlock->size += sizeof(block_t) + newBlock->next->size;
     }
 }
 
-int memgetminimumsize()
-{
-    return sizeof(block_t);
-}
-
-int memgetblocksize()
-{
-    return 0;
+void memdone() {
+  memList = NULL;
+  memSize = 0;
+  init = FALSE;
 }
--- Sergey/memallocator/memallocator.h
+++ Sergey/memallocator/memallocator.h
@@ -1,14 +1,68 @@
-#pragma once
+#ifndef __MEMALLOCATOR_H__E71A34CB
+#define __MEMALLOCATOR_H__E71A34CB
 
-typedef struct block_t {
-  int size;
-  struct block_t* next;
-  struct block_t* prev;
-} block_t;
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-int meminit(void* pMemory, int size);
-void memdone();
-void* memalloc(int size);
-void memfree(void* p);
-int memgetminimumsize();
-int memgetblocksize();
+  enum {
+    ERROR = -1,
+    OK = 1
+  };
+
+  typedef enum {
+    FALSE,
+    TRUE
+  } bool_t;
+
+  typedef struct block_t {
+    int size;
+    struct block_t* next;
+    struct block_t* prev;
+  } block_t;
+
+  // Init memory system with memory block pMemory.
+  int meminit(void* pMemory, int size);
+
+  // You can implement memory leak checks here
+  void memdone();
+
+  // Allocate memory block of size 'size'.
+  // Returns pointer to memory block is success, 0 otherwise
+  void* memalloc(int size);
+
+  // Free memory previously allocated by memalloc
+  void memfree(void* p);
+
+  // Return minimum size in bytes of the memory pool to allocate 0-bytes block
+  // use case:
+  // void *p   = 0;
+  // void *ptr = malloc(memgetminimumsize() + 1);
+  // meminit(ptr, memgetminimumsize() + 1)
+  // p = memalloc(1); // Success!
+  // memfree(p);
+  // memdone();
+  // free(ptr);
+  int memgetminimumsize();
+
+
+  // Returns size in bytes of additional information per allocation
+  // use case:
+  // void *p1 = 0, *p2 = 0;
+  // int  memsize = memgetminimumsize() + memgetblocksize() + 2;
+  // void *ptr = malloc(memsize);
+  // meminit(ptr, memsize())
+  // p1 = memalloc(1); // Success!
+  // p2 = memalloc(1); // Success!
+  // memfree(p2);
+  // memfree(p1);
+  // memdone();
+  // free(ptr);
+  int memgetblocksize();
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
